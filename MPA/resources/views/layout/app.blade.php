ee5d762384a54c747685797ad1da50dec0b4dede
DKamman <42833740+DKamman@users.noreply.github.com>
2021-01-19 14:51:27 +0100

    Intial commit, inc db migrations, inc bootstrap

---
+++ MPA/resources/views/layout/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="nav">
+            <div class="nav-link">
+                <a class="" href="">HOME</a>
+            </div> 
+        </div>
+
+        <div class="main">
+            @yield('content')
+        </div>
+    </body>
+</html>
---
+++ MPA/resources/views/products/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    
+@endsection
