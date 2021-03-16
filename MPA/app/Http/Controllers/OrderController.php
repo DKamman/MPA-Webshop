20fc23d69bfb75258e89042cbffd449b5c794200
DKamman <42833740+DKamman@users.noreply.github.com>
2021-03-16 17:33:09 +0100

    Created OrderController and Order model

---
+++ MPA/app/Http/Controllers/OrderController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    
+    /* Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+   public function order(Request $request)
+   {
+       if ($request->session()->get('cart')) {
+           $user = Auth::user(); 
+           $cart = $request->session()->get('cart');
+
+           Order::create([
+               'cart' => serialize($cart),
+               'user_id' => $user->id,
+           ]);
+
+       }
+       $request->session()->forget('cart');
+
+       return redirect()->route('shop.index');
+   }
+
+   /* Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+   public function show($id)
+   {
+       $order = Order::find($id);
+
+       if ($order !== null && Auth::user()->id == $order->user_id || Auth::user()->is_admin){
+           return view('admin.order.show',[
+               'order'=>$order,
+               'cart'=>unserialize($order->cart),
+               ]);
+       }
+
+       return redirect()->route('main.index');
+   }
+
+}
