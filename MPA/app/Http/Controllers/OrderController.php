c6f16dc17450da0e3ed04d2ccfe560cef695aed1
DKamman <42833740+DKamman@users.noreply.github.com>
2021-04-07 15:33:52 +0200

    User can place Order

--- MPA/app/Http/Controllers/OrderController.php
+++ MPA/app/Http/Controllers/OrderController.php
@@ -3,20 +3,34 @@
 namespace App\Http\Controllers;
 use Auth;
 use App\Models\Order;
+use App\Models\OrderProduct;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
 
    public function order(Request $request)
-   {
-
-    $user = Auth::user(); 
+   { 
  
-    if ($user != NULL) {
+    if (Auth::user() != NULL) {
         if ($request->session()->get('cart')) {
-            $order = new Order($request);
-            $order->getKeys();        
+            $orderId;
+            $cart = $request->session()->get('cart')->items;
+
+            //Creates Order, saves ID
+            $newOrder = Order::create([
+                'user_id' => auth()->id(),
+            ]);
+            $orderId = $newOrder->id;
+
+            foreach ($cart as $item) {
+                OrderProduct::create([
+                    'order_id' => $orderId,
+                    'product_id' => $item['item']['id'],
+                    'quantity' => $item['qty'],
+                    'price' => $item['price']
+                ]);
+            }
         }
     } else {
        return redirect()->route('register');
--- MPA/app/Models/Order.php
+++ MPA/app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Order extends Model
 {
@@ -13,48 +14,11 @@ class Order extends Model
         'cart',
         'user_id'
     ];
-
-    //Order ID
-    public $orderId;
-    public $orderItems = [];
-    public $productKeys;
-
-    public function __construct($request) {
-
-        $cart = $request->session()->get('cart')->items;
-        $productKeys = array_keys($cart);
-
-        // Creates Order, saves ID
-        // $order = Order::create([
-        //     'user_id' => $user->id,
-        // ]);
-        // $orderId = $order->id;
-    }
-
-    public function getCartItems() {
-
-        // ID's
-        foreach ($productKeys as $item) {
-            echo $item;
-        }
-
-        //Amount and Price
-        foreach ($cart as $item) {
-            echo $item['qty'];
-            echo $item['price'];
-        } 
-
-        // Possible solution for getting Item
-        // foreach($this->productKeys as $item) {
-        //     if (array_key_exists($item, $this->productKeys)) {
-        //         echo $item['qty'] . ', ' . $item['price'] . '</br>';
-        //     }
-        // }
-    }
-
-    public function getKeys() {
-        foreach ($this->productKeys as $id) {
-            echo $id . ", ";
-        }
-    }
+    /**
+    * Get the order products for the order.
+    */
+    // public function orderProducts()
+    // {
+    //     return $this->hasMany(OrderProduct::class);
+    // }
 }
